1ed59f7cb75dad0c768822eb4ccf1833e5b461ed
Aliimxm <105807082+Aliimxm@users.noreply.github.com>
2022-06-20 18:14:18 +0530

    Add files via upload

--- timer.js
+++ timer.js
@@ -11,6 +11,15 @@ function pause()
 {
     clearInterval(timeOut);
 }
+function reset()
+{
+    mils="00";
+    ms=$("h1.ms").text(mils.toString());
+    seconds="00";
+    sec=$("h1.sec").text(seconds.toString());
+    mins="00";
+    min=$("h1.min").text(mins.toString());
+}
 function start()
 {
     timeOut=setInterval(function ()
@@ -33,5 +42,34 @@ function start()
     }
 },10);
 
+}
+var ts;
+var th;
+var tm;
+var timerSeconds=$("card-body.s").text(ts.toString());
+var timerMins=$("card-body.m").text(tm.toString());
+var timerHours=$("card-body.h").text(th.toString());
+function timer()
+{
+
+    timeOut=setInterval(function ()
+    {
+        timerSeconds--;
+        ts=$("h1.seconds").text(timerSeconds.toString());
+        if(ts===0)
+        {
+            timerSeconds=0;
+            timerMins--;
+            tm=$("h1.mi").text(timerMins.toString());
+    
+        }
+        if(tm===0)
+        {
+           timerMins=0;
+            timerHours--;
+           th=$("h1.min").text(timerHours.toString());
+    
+        }
+    },100);
 }
 
